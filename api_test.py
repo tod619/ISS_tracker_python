a0650d734698b196c7104bf11c0d988f572f6dab
tod619 <trevorodwyer619@hotmail.com>
2023-07-24 22:13:42 +0100

    get latitude info from response.json in api_test file

--- api_test.py
+++ api_test.py
@@ -5,5 +5,6 @@ import requests
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
-data = response.json()["iss_position"]["longitude"]
-print(data)
+longitude = response.json()["iss_position"]["longitude"]
+latitude = response.json()["iss_position"]["latitude"]
+print(longitude, latitude)
