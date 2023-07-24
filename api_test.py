795441393873b096a19195f94fec303176169f5d
tod619 <trevorodwyer619@hotmail.com>
2023-07-24 22:10:45 +0100

    Get iss longitude data from data json response in main.py file

--- api_test.py
+++ api_test.py
@@ -3,4 +3,7 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
+
+data = response.json()["iss_position"]["longitude"]
+print(data)
