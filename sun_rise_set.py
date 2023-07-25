1859e90283c4ded981769ff407f00befda6eb510
tod619 <trevorodwyer619@hotmail.com>
2023-07-25 21:45:35 +0100

    Add parameters with lat + long to find sunset in sun_rise_set.py file

--- sun_rise_set.py
+++ sun_rise_set.py
@@ -1,6 +1,15 @@
 import requests
 
-response = requests.get("https://api.sunrise-sunset.org/json")
+MY_LAT = 51.507351
+MY_LNG = -0.127758
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+}
+
+response = requests.get(
+    "https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
 print(data)
