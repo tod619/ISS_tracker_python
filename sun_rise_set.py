c791b5f211ab0170108629e2925a0632fd3ebe9a
tod619 <trevorodwyer619@hotmail.com>
2023-07-25 22:01:52 +0100

    Get sunrise, sunset + time_now hour value

--- sun_rise_set.py
+++ sun_rise_set.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 MY_LAT = 51.507351
 MY_LNG = -0.127758
@@ -6,12 +7,16 @@ MY_LNG = -0.127758
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LNG,
+    "formatted": 0,
 }
 
 response = requests.get(
     "https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 print(sunrise, sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
