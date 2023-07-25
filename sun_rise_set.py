f650056f56c726df9ffc955a856344bfad4f04ed
tod619 <trevorodwyer619@hotmail.com>
2023-07-25 21:50:46 +0100

    get sunrise + sunset data from response

--- sun_rise_set.py
+++ sun_rise_set.py
@@ -12,4 +12,6 @@ response = requests.get(
     "https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise, sunset)
